7a837bf0a7931c34fc465eac18ce6304790b1e87
Hizbawi-Sisay <ashenhizb@gmail.com>
2022-03-25 00:01:29 +0300

    sec-pointer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,42 +2,24 @@
 #include <stdio.h>
 
 /**
- * *cap_string - capitalize words.
+ * *cap_string - this is awesome
+ * @s: pointer to char params
  *
- * @str: hold character.
- * Return: string.
+ * Return: *s
  */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+	char delimeters[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0)
-		{
-			if ((str[i] >= 97 && str[i] <= 122))
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else if (str[i] == ' ')
-		{
-			++i;
-			if (str[i] >= 97 && str[i] <= 122)
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (str[i] >= 65 && str[i] <= 90)
-			{
-				str[i] = str[i] + 32;
-			}
-		}
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] = s[0] - 32;
+				for (j = 0; delimeters[j] != '\0'; j++)
+					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
 	}
-	return (str);
+	return (s);
 }
